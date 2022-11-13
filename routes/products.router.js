9056e7c43d570960e9c50d2b798ff4dd1fb7226b
fabio4520 <fabioleofc@gmail.com>
2022-11-13 07:13:48 -0500

    ADD: faker dependency Problem - There is a problem with faker v6 Solution - Add faker-js/faker dependency according to this solution in stackoverflow https://stackoverflow.com/questions/21866285/cannot-find-module-faker-after-npm-install-save-dev

--- routes/products.router.js
+++ routes/products.router.js
@@ -1,8 +1,20 @@
 const express = require('express');
-const faker = require('faker');
+const {faker} = require('@faker-js/faker');
 
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  
-})
+  const products = [];
+  const { size } = req.query;
+  const limit = size || 10;
+  for (let index = 0; index < limit; index++) {
+    products.push({
+      name: faker.commerce.productName(),
+      price: parseInt(faker.commerce.price(), 10),
+      image: faker.image.imageUrl(),
+    });
+  }
+  res.json(products);
+});
+
+module.exports = router;
