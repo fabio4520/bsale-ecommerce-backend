536a4d9c340dfe2aa94fdc5e9bdf81cbe1ddcade
fabio4520 <fabioleofc@gmail.com>
2022-11-13 08:09:14 -0500

    add services for products

--- routes/products.router.js
+++ routes/products.router.js
@@ -1,54 +1,61 @@
 const express = require('express');
-const {faker} = require('@faker-js/faker');
 
+const ProductsService = require("../service/product.service");
+const service = new ProductsService();
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  const products = [];
-  const { size } = req.query;
-  const limit = size || 10;
-  for (let index = 0; index < limit; index++) {
-    products.push({
-      id: index,
-      name: faker.commerce.productName(),
-      price: parseInt(faker.commerce.price(), 10),
-      image: faker.image.imageUrl(),
-    });
-  }
+  const products = service.findAll();
   res.json(products);
 });
 
 router.get('/:id', (req, res) => {
-  const { id } = req.query;
-  res.json({
-    id
-  })
+  try {
+    const { id } = req.params;
+    const product = service.findOne(id);
+    res.json(product)
+  } catch (error) {
+    res.status(404).json({
+      message: error.message
+    })
+  }
 })
 
 router.post('/', (req, res) => {
-  const body = req.body;
-  res.json({
-    message: 'created',
-    data: body
-  });
+  try {
+    const body = req.body;
+    const newProduct = service.create(body);
+    res.status(201).json(newProduct);
+  } catch (error) {
+    res.status(404).json({
+      message: error.message
+    })
+  }
 })
 
 router.patch('/:id', (req, res) => {
-  const { id } = req.params;
-  const body = req.body;
-  res.json({
-    message: 'update',
-    data: body,
-    id,
-  });
+  try {
+    const { id } = req.params;
+    const body = req.body;
+    const product = service.update(id, body);
+    res.json(product); 
+  } catch (error) {
+    res.status(404).json({
+      message: error.message
+    })
+  }
 });
 
 router.delete('/:id', (req, res) => {
-  const { id } = req.params;
-  res.json({
-    message: 'deleted',
-    id,
-  });
+  try {
+    const { id } = req.params;
+    const answer = service.delete(id);
+    res.json(answer); 
+  } catch (error) {
+    res.status(404).json({
+      message: error.message
+    })
+  }
 });
 
 module.exports = router;
