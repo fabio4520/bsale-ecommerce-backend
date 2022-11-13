0e01f15c2b0a894fb6d7d14ea59775f7f06ea779
fabio4520 <fabioleofc@gmail.com>
2022-11-13 10:12:20 -0500

    handle reconnection to database

--- config/config.js
+++ config/config.js
@@ -1,19 +1,29 @@
 const mysql = require('mysql');
 
-const connection = mysql.createConnection({
+const db_config = {
+  connectionLimit: 10,
   host: 'mdb-test.c6vunyturrl6.us-west-1.rds.amazonaws.com',
   user: 'bsale_test',
   password: 'bsale_test',
   port: 3306,
   database: 'bsale_test'
-})
+}
 
-connection.connect(function (err) {
+const connection = mysql.createPool(db_config);
+connection.getConnection((err, connection) => {
   if (err) {
-    console.log('Database connection failed: ' + err.stack);
-    return
-  }
-  console.log('Connected to database');
+    if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+        console.error('Database connection was closed.')
+    }
+    if (err.code === 'ER_CON_COUNT_ERROR') {
+        console.error('Database has too many connections.')
+    }
+    if (err.code === 'ECONNREFUSED') {
+        console.error('Database connection was refused.')
+    }
+}
+  if (connection) connection.release()
+  return
 })
 
 module.exports = connection;
